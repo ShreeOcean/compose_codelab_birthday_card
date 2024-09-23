08d16e967bca94025801f60182b7b2c49a498792
PRAGYANSHREE DAS <pragyan9423@gmail.com>
2024-09-23 14:18:45 +0530

    todo insert image and run

--- app/src/main/java/com/ocean/mybdcard/MainActivity.kt
+++ app/src/main/java/com/ocean/mybdcard/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +18,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,14 +44,34 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(
+                    /*GreetingText(
                         msg = "Happy Birthday Ocean!",
                         from = "From Emma",
                         modifier = Modifier.padding(8.dp)
-                    )
+                    )*/
+                    GreetingImage(msg = stringResource(id = R.string.happy_birthday_text), from = stringResource(R.string.signature_text), modifier = Modifier.padding(8.dp) )
                 }
             }
         }
+    }
+}
+
+@Composable
+fun GreetingImage(msg: String, from: String, modifier: Modifier) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        GreetingText(
+            msg = msg, 
+            from = from, 
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
     }
 }
 
@@ -82,6 +107,9 @@ fun GreetingText(msg: String, from: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyBDcardTheme {
-        GreetingText("Happy Birthday Ocean", "From Emma")
+//        GreetingText("Happy Birthday Ocean", "From Emma")
+        GreetingImage(msg = stringResource(id = R.string.happy_birthday_text), from = stringResource(
+            id = R.string.signature_text
+        ), modifier = Modifier)
     }
 }
